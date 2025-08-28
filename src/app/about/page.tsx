054c17b9a99f6113acbf2ac12f51697aa3c61833
Hassan <hassan@gmail.com>
2025-08-28 05:55:33 -0700

    About Page Done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -56,6 +56,38 @@ const page = () => {
         <ShinyText className='max-w-[80ch] text-[clamp(0.7rem,1vw,1.125rem)] text-gray-100/60' text='After years in the industry, I realized that my greatest joy came not just from creating, but from sharing knowledge. VFX can often seem intimidating, filled with complex tools and technical jargon. My mission is to demystify visual effects and make them accessible to anyone with passion and creativity. Through my courses, I focus on hands-on, practical training designed to help aspiring artists build professional, industry-ready portfolios.'/>
       </div>
 
+      <div className='p-[clamp(1rem,2vw,2.5rem)] mt-3'>
+        <div className='overflow-hidden flex items-center gap-1 sm:gap-4 md:gap-4'>
+          <h1 className='text-[clamp(1.35rem,5vw,4rem)] text-main font-extrabold'>Ready to Learn? </h1>
+        </div>
+
+        <ShinyText className='max-w-[80ch] text-[clamp(0.7rem,1vw,1.125rem)] text-gray-100/60' text='If you’re serious about mastering VFX and want to learn from someone who’s been on the front lines of blockbuster filmmaking, I invite you to explore my courses and portfolio. Let’s unlock your potential and turn your passion for VFX into a professional reality.'/>
+
+        <div className='w-screen flex items-center [@media(max-width:520px)]:justify-center mt-5'>
+          <a
+          href="/courses"
+          className="
+            inline-flex justify-center items-center relative h-[50px] px-[30px] border-2 border-[#FFB400] bg-[#1A1A1A]
+            select-none whitespace-nowrap transition-all duration-75 ease-linear
+            font-inherit no-underline
+            before:content-[''] before:absolute before:bg-[#1A1A1A]
+            before:transition-all before:duration-200
+            before:w-[calc(100%+6px)] before:h-[calc(100%-16px)] before:top-[8px] before:left-[-3px]
+            after:content-[''] after:absolute after:bg-[#1A1A1A]
+            after:transition-all after:duration-200
+            after:w-[calc(100%-16px)] after:h-[calc(100%+6px)] after:top-[-3px] after:left-[8px]
+            hover:cursor-crosshair active:scale-95
+            hover:before:h-[calc(100%-32px)] hover:before:top-[16px]
+            hover:after:w-[calc(100%-32px)] hover:after:left-[16px]
+          "
+        >
+          <span className="relative z-[3] font-semibold text-[15px] text-[#FFB400]">
+            GAME ON
+          </span>
+        </a>
+      </div>
+      </div>
+
     </div>
   )
 }
